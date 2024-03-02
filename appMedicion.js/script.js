5de2d62a4bb30d36e4d3ecec7b4a763e94f92a9e
DavidParedesV20 <145064481+DavidParedesV20@users.noreply.github.com>
2024-03-02 10:15:38 -0500

    Add files via upload

---
+++ appMedicion.js/script.js
@@ -0,0 +1,33 @@
+/*documento script*/
+var btn = document.getElementById('button');
+var resultados = document.getElementById('resultInfo');
+
+var producto = document.getElementById('producto');
+var peso = document.getElementById('peso');
+var calorias = document.getElementById('calorias');
+
+btn.addEventListener('click', function(){
+    var infoProdu = producto.value;
+    var infoPeso = peso.value;
+    var infoCal = calorias.value;
+
+
+    /* IMPRIME EL PRODUCTO*/
+    if(infoProdu.trim() !== ''){
+        var nuevaInfoProd = document.createElement('li');
+        nuevaInfoProd.textContent = infoProdu;
+
+        resultados.appendChild(nuevaInfoProd);
+        infoProdu.value = '';
+    }
+
+    /* IMPRIME EL PESO */
+    if(infoPeso.trim() !== ''){
+    var nuevaInfoPeso = document.createElement('li');
+    nuevaInfoPeso.textContent = infoPeso;
+
+    resultados.appendChild(nuevaInfoPeso);
+    infoPeso.value = '';
+
+    }
+});
