e920ddcc3fb6aacc2a48d5246e932486c9dd9a4a
DavidParedesV20 <145064481+DavidParedesV20@users.noreply.github.com>
2024-03-02 20:59:51 -0500

    Update script.js

--- appMedicion.js/script.js
+++ appMedicion.js/script.js
@@ -5,11 +5,13 @@ var resultados = document.getElementById('resultInfo');
 var producto = document.getElementById('producto');
 var peso = document.getElementById('peso');
 var calorias = document.getElementById('calorias');
+var pesoEsperado = document.getElementById('pesoEsperado');
 
 btn.addEventListener('click', function(){
     var infoProdu = producto.value;
     var infoPeso = peso.value;
     var infoCal = calorias.value;
+    var user = pesoEsperado.value;
 
 
     /* IMPRIME EL PRODUCTO*/
@@ -22,12 +24,17 @@ btn.addEventListener('click', function(){
     }
 
     /* IMPRIME EL RESUTADO */
-    if(infoPeso.trim() !== ''){
-    var nuevaInfoPeso = document.createElement('li');
-    nuevaInfoPeso.textContent = (100 * infoCal) /infoPeso;
+    if(infoPeso.trim() !== '' && user.trim() !== ''){
+    var nuevaInfoPeso = document.createElement('h3');
+    nuevaInfoPeso.textContent = `Hay ${(user * infoCal) / infoPeso} Cal en ${user} gramos`;
+
+    nuevaInfoPeso.classList.add('resultItem');
 
     resultados.appendChild(nuevaInfoPeso);
     infoPeso.value = '';
-
+    producto.value = '';
+    peso.value = '';
+    calorias.value = '';
+    pesoEsperado.value = '';
     }
 });
